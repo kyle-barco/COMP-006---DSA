4f15694c82b2133e8875ad1c1d30561c2fd3a0a3
kyle-barco <kylebarco04@gmail.com>
2025-10-23 20:11:27 +0800

    add second solution

--- 01_activity2/activity2.js
+++ 01_activity2/activity2.js
@@ -1,12 +1,32 @@
 let arr = [1,2,3,4,5,6,7,8,9,0]
 
-function isNumExists(array, num ) {
-  for (let i = 0; i < array.length; i++) {
-    if(array[i] === num) {
-     return `Number "${num}" exists: found in index "${array.indexOf(num)}" ` 
-    }       
+function binarySearch(array, num) {
+  let left = 0, right = array.length - 1
+    
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2)
+    
+    if(array[middle] == num) {
+      return middle
+    } else if (array[middle] < num) {
+      left = middle + 1
+    } else {
+      right = middle - 1
+    }
   }
-  return `Number "${num}" does not found`
+  return -1 
 }
-console.log(isNumExists(arr, 7))
+
+function output(arr, num) {
+  let index = binarySearch(arr, num)
+    if(!index == -1) {
+      return `Index of "${num}": "${index}"`
+    } else {
+      return `Number "${num}" is not in the list`
+    }
+}
+
+console.log(output(arr, 7))
+
+
 
