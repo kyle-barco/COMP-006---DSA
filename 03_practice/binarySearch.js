c27ec81f8dea6dc99526ff860e76fd18809f90ad
kyle-barco <kylebarco04@gmail.com>
2025-12-31 09:08:25 +0800

    update for practice the binarySearch

--- 03_practice/binarySearch.js
+++ 03_practice/binarySearch.js
@@ -1,10 +1,18 @@
+let arrNum = [1,2,3,4,5,6,7,8,9]
 
+function binarySearch (arr, num) {
+    let left = 0
+    let right = arr.length - 1
 
-let arr = [1,2,3,4,5]
-function binarySearch() {
-    if (high >= low) {
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2)
 
+        if (arr[mid] == num) {
+            return num
+        } else if (arr[mid] < num) {
+            left = mid + 1;
+        } else {
+            right = mid - 1
+        }
     }
 }
-
-console.log(arr)
