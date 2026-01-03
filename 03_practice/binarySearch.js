b55bd9a663d5e306733206b8fab8e88220f9b115
kyle-barco <kylebarco04@gmail.com>
2026-01-03 10:43:03 +0800

    done practicing binarySearch

--- 03_practice/binarySearch.js
+++ 03_practice/binarySearch.js
@@ -1,16 +1,16 @@
 let arrNum = [1,2,3,4,5,6,7,8,9]
 
 function binarySearch(arr, num) {
-    let left = 0;
-    let right = arr.length - 1;
+    let left = 0
+    let right = arr.length - 1
 
     while (left <= right) {
         let mid = Math.floor((left + right) / 2)
 
-        if (arr[mid] === num) {
+        if(arr[mid] == num) {
             return mid
-        } else if (arr[mid] < num) {
-            left = mid + 1;
+        } else if(arr[mid] < num) {
+            left = mid + 1
         } else {
             right = mid - 1
         }
@@ -18,10 +18,10 @@ function binarySearch(arr, num) {
     return -1
 }
 
-const CHOSEN_NUM = 7
-const idx = binarySearch(arrNum, CHOSEN_NUM)
+const CHOSEN_NUM = 4
+let idx = binarySearch(arrNum, CHOSEN_NUM)
 
-if (idx !== -1) {
+if(idx !== -1) {
     console.log(`Number found at index: ${idx}`)
 } else {
     console.log(`Number not found`)
