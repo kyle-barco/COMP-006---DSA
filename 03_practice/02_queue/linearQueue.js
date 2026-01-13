546afdf7c40c2fb14bfa3412b33a1428d956f459
kyle-barco <kylebarco04@gmail.com>
2026-01-13 09:36:19 +0800

    practice

--- 03_practice/02_queue/linearQueue.js
+++ 03_practice/02_queue/linearQueue.js
@@ -1,5 +1,5 @@
 class Queue {
-    constructor() {
+    constructor(){
         this.cap = 4
         this.limit = this.cap
         this.array = new Array(this.cap)
@@ -7,39 +7,38 @@ class Queue {
         this.rear = -1
     }
 
-    isEmpty() {
-        if (this.front == -1 && this.rear == -1) {
+    isEmpty(){
+        if(this.front == -1 && this.rear == -1){
             return true
         } else {
             return false
         }
     }
 
-    isFull() {
-        if (this.rear == this.limit - 1) {
+    isFull(){
+        if(this.rear == this.limit - 1){
             return true
         } else {
             return false
         }
     }
 
-    enqueue(value) {
-        if (this.isFull()) {
+    enqueue(value){
+        if(this.isFull()){
             console.log("full queue");
-        } else if (this.isEmpty()) {
+        } else if(this.isEmpty()){
             this.front = 0
             this.rear = 0
-            this.array[this.rear] = value
+            this.array[this.rear] = value 
         } else {
             this.rear += 1
             this.array[this.rear] = value
         }
     }
 
-    dequeue() {
-        if (this.isEmpty()) {
+    dequeue(){
+        if(this.isEmpty()){
             console.log("empty queue");
-            return
         }
         this.array[this.front] = "None"
         this.front += 1
@@ -50,7 +49,7 @@ class Queue {
         }
     }
 
-    peak() {
+    peak(){
         return this.array[this.front]
     }
 }
@@ -60,6 +59,7 @@ myQueue.enqueue(1)
 myQueue.enqueue(2)
 myQueue.enqueue(3)
 myQueue.enqueue(4)
+myQueue.dequeue()
 console.log(`Array: ${myQueue.array}`)
 console.log(`isEmpty: ${myQueue.isEmpty()}`);
 console.log(`isFull: ${myQueue.isFull()}`);
